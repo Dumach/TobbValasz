48021890e57205d15d82f5b795e025dd9d248d3d
Alex Nagy <nagyalex003@gmail.com>
2025-04-02 16:21:16 +0200

    feat: added homepage and instructions

--- script.js
+++ script.js
@@ -1,25 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const testMenu = document.getElementById('quiz-nav');
   const quizContainer = document.getElementById('quiz-container');
   const quizTitle = document.getElementById('quiz-title');
   const questionContainer = document.getElementById('question-container');
 
-  const IMAGE_BASE_PATH = 'tests/.net_programozas/'; // Global variable for image base path
-
   let currentQuestionIndex = 0;
   let quizData = null;
   let statusBar = null;
+  let IMAGE_BASE_PATH = '';
 
-  // Load quiz data
-  fetch('tests/.net_programozas/test.json')
+  const homeButton = document.getElementById('home-button');
+  homeButton.addEventListener('click', () => {
+    quizTitle.textContent = 'Select a Test';
+    questionContainer.style.textAlign = 'left';
+    questionContainer.innerHTML = `<ol>
+      <li>Update the <strong>tests/tests.json</strong> file with the new test details.</li>
+      <li>Add the test file to the <strong>tests</strong> folder.</li>
+      <li>Ensure the test file is in the correct JSON format.</li>
+      <li>Click on the test name in the menu to start the quiz.</li>
+    </ol>`;
+    currentQuestionIndex = 0;
+  });
+
+  // Call homepage first
+  homeButton.click();
+
+  // Load available tests
+  fetch('tests/tests.json')
     .then((response) => response.json())
     .then((data) => {
-      quizData = data;
-      quizTitle.textContent = quizData.title;
-      showQuestion();
+      data.tests.forEach((test) => {
+        const menuItem = document.createElement('a');
+        menuItem.textContent = test.title;
+        menuItem.href = '#';
+        menuItem.className = 'menu-item';
+        menuItem.addEventListener('click', () => loadTest(test.path));
+        testMenu.appendChild(menuItem);
+      });
     })
     .catch((error) => {
-      console.error('Error loading quiz data:', error);
+      console.error('Error loading tests:', error);
     });
+
+  function loadTest(testPath) {
+    questionContainer.style.textAlign = 'center';
+    IMAGE_BASE_PATH = testPath.substring(0, testPath.lastIndexOf('/') + 1); // Update base path dynamically
+    fetch(testPath)
+      .then((response) => response.json())
+      .then((data) => {
+        quizData = data;
+        quizTitle.textContent = quizData.title; // Update title correctly
+        currentQuestionIndex = 0;
+        showQuestion();
+      })
+      .catch((error) => {
+        console.error('Error loading quiz data:', error);
+      });
+  }
 
   // Show a question
   function showQuestion() {
